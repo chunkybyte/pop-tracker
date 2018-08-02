5dd06ec4cd01dc432b356c3d2dd602ff54154af6
C Hunky <nerdlordchunky@gmail.com>
2018-08-02 23:24:43 +0530

    OMDB Search Functionality

--- src/components/SearchPop.js
+++ src/components/SearchPop.js
@@ -1,51 +1,79 @@
 import React from 'react';
 
 class SearchPop extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResult: null
+        };
+    }
+
     giveResults = (inp) => {
+        if (inp.length === 0) {
+            this.setState({
+                searchResult: null
+            })
+        }
+
         if (inp.length >= 3) {
             console.log(inp);
             fetch(`http://www.omdbapi.com/?apikey=4aaf9e6a&t=${inp}&plot=short&r=json`)
                 .then(res => res.json())
                 .then(res => {
-                    this.render(res);
+                    console.log(res);
+                    if (res.Response === 'False') {
+                        res = {
+                            Response: "NAN"
+                        };
+                    }
+                    this.setState({
+                        searchResult: res
+                    });
                 });
         }
     }
 
-    renderResult = (res) => {
-        if (res) {
-            console.log('renderResult');
-            console.log(res);
+    renderResult = () => {
+        let data = this.state.searchResult;
+        if (data.Response === "NAN") {
+            console.log('Null');
             return (
-                <div id="searchResult">
-                    <div className="resultImg">
-                        <img src={res.Poster} alt={res.Title}/>
-                    </div>
-                    <div>
-                        <h4>
-                            {res.Title}
-                        </h4>
-                    </div>
+                <div id="searchResult" style={{justifyContent:"center"}}>
+                    No Result Found!
                 </div>
-            );
-        } else {
-            console.log('No Result to Show');
-            return <div>Nothing</div>;
+            )
         }
+        console.log('renderResult data');
+        console.log(data);
+
+        return (
+            <div id="searchResult">
+                <div className="resultImg">
+                    <img src={data.Poster === 'N/A' ? "https://dummyimage.com/150x150/f5f5f5/0919f0&text=Poster+Unavailable" : data.Poster} alt={data.Title}/>
+                </div>
+                <div className="resultInfo">
+                    <strong>
+                        {data.Title} <i>({data.Year})</i>
+                    </strong>
+                    <p>
+                        Starring : {data.Actors} <br/>
+                        Rated : {data.imdbRating === 'N/A' ? "Unrated" : `${data.imdbRating} / 10`} <br/>
+                        Runtime : {data.Runtime} <br/>
+                        Country : {data.Country} <br/>
+                    </p>
+                </div>
+            </div>
+        );
     }
 
-    render(res) {
-        if(res) {
-            return this.renderResult(res);
-        } else {
-            return (
-                <div id="searchBox">
-                    <h3>Search Movies, TV</h3>
-                    <input type="text" name="search" id="searchArea" placeholder="Start typing the name of the show, movies, etc you're searching..." onInput={(e) => {this.giveResults(e.target.value)}} />
-                    {res ? this.renderResult(res) : <div>Nothing</div>}
-                </div>
-            );
-        }
+    render() {
+        return (
+            <div id="searchBox">
+                <h3>Search Movies, TV</h3>
+                <input type="text" name="search" id="searchArea" placeholder="Start typing the name of the show, movies, etc you're searching..." onInput={(e) => {this.giveResults(e.target.value)}} />
+                {this.state.searchResult == null ? "" : this.renderResult()}
+            </div>
+        );
     }
 }
 
